929262613d9f282e3091c2a397105cd985310b5c
Sudarshan Wadkar <wadkar@users.noreply.github.com>
2020-04-08 02:20:19 +0530

    Add CoronaApp model

--- gocorona_api/gocorona_api/users/models.py
+++ gocorona_api/gocorona_api/users/models.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, Model
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+from django_pandas.managers import DataFrameManager
+
+# Create your models here.
+#"UUID","Status","Lat","Lng"
+
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
 class User(AbstractUser):
@@ -12,3 +23,20 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+class CoronaApp(Model):
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    name = models.CharField(max_length=100, blank=True, default='a')
+    status = models.IntegerField(default=0)
+
+    latitude= models.FloatField(default=0)
+    longitude= models.FloatField(default=0)
+
+    objects = DataFrameManager()
+
+    class Meta:
+        ordering = ['created']
+
+
