d319a209f86f437e286158d88c0e47b1df9a87d6
Sudarshan Wadkar <wadkar@users.noreply.github.com>
2020-04-08 02:37:42 +0530

    Add serializer for CoronaApp

--- gocorona_api/gocorona_api/users/api/serializers.py
+++ gocorona_api/gocorona_api/users/api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
-from gocorona_api.users.models import User
-
+from gocorona_api.users.models import User, CoronaApp, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +10,32 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
         }
+
+
+class CoronaAppSerializer(serializers.Serializer):
+
+    name = serializers.CharField(max_length=100, default='a')
+    status = serializers.IntegerField(default=0)
+
+    latitude= serializers.FloatField(default=0)
+    longitude= serializers.FloatField(default=0)
+    #required=True, 
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return CoronaApp.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.status = validated_data.get('status', instance.status)
+        instance.latitude = validated_data.get('latitude', instance.latitude)
+        instance.longitude = validated_data.get('longitude', instance.longitude)
+
+        instance.save()
+
+        return instance
