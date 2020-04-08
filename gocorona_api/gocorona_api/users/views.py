1055c19a87a00b27f69be841a099eaa40ab8609d
Sudarshan Wadkar <wadkar@users.noreply.github.com>
2020-04-08 00:54:12 +0000

    Fix intersection calculator

--- gocorona_api/gocorona_api/users/views.py
+++ gocorona_api/gocorona_api/users/views.py
@@ -57,21 +57,22 @@ class CoronaAppDetail(mixins.RetrieveModelMixin,
 
 class CoronaAppResult(APIView):
 
-    def get(self, request, format=None):
+    def get(self, request, format=None, username=None):
         data = CoronaApp.objects.all()
         data_frame = read_frame(data)            # Transform queryset into pandas dataframe 
         data_frame.rename(columns={'name': 'UUID', 'status': 'Status', 'latitude': 'Lat', 'longitude': 'Lng'}, inplace=True)
         data_frame.drop(['id', 'created'], axis=1)
 
         # ISSUE DETECTED IN 89: drop.['Status_1','UUID_1','Lat_1','Lng_1'] NOT FOUND IN AXIS. UNCOMMENT FOLLOWING LINES ONCE ERROR RESOLVED
-        #infected = self.intersection_calculator(data_frame)
-        #return Response(infected)
+        infected = self.intersection_calculator(data_frame)
+        return Response(infected)
 
 
-        return Response(data_frame)              # Return the result in JSON via Django REST Framework
+        #return Response(data_frame)              # Return the result in JSON via Django REST Framework
 
     def intersection_calculator(self, data_frame):
-        allPts = GeoDataFrame(data_frame,crs={"init":"EPSG:4326"},geometry=[Point(xy) for xy in zip(data_frame["Lat"], data_frame["Lng"])]).to_crs("EPSG:3395")
+        allPts = GeoDataFrame(data_frame,crs={"init":"EPSG:4326"},geometry=[Point(xy) for xy in zip(data_frame["Lat"], data_frame["Lng"])]).to_crs("EPSG:3310")
+        #EPSG == 3310 for distance in meters
         positivePoints = allPts[allPts['Status']==1]
         negativePoints = allPts[allPts['Status']==0]
 
@@ -79,13 +80,15 @@ class CoronaAppResult(APIView):
         #print(negativePoints)
 
         positivePointsBuffered = positivePoints.set_geometry(positivePoints.geometry.buffer(20,resolution=16),inplace=False)
-        negativePointsBuffered = negativePoints.set_geometry(negativePoints.geometry.buffer(20,resolution=16),inplace=False)
+        #negativePointsBuffered = negativePoints.set_geometry(negativePoints.geometry.buffer(20,resolution=16),inplace=False)
         #print(positivePointsBuffered)
 
         #ax=positivePointsBuffered.plot(color="red")
         #negativePointsBuffered.plot(ax=ax,color='green', alpha=0.5)
 
-        #intersecting_points = negativePoints.intersection(positivePointsBuffered.unary_union)
+        intersecting_points = negativePoints.within(positivePointsBuffered.unary_union)
+        res_intersect = negativePoints[intersecting_points]
+
         #print(intersecting_points)
         #print(negativePoints)
 
@@ -93,15 +96,15 @@ class CoronaAppResult(APIView):
         #combined_infectious_points = positivePointGeometryBuffered.unary_union
 
 
-        res_intersect = overlay(positivePointsBuffered,negativePointsBuffered,how='intersection')
-        res_intersect=res_intersect.drop(['Status_1','UUID_1','Lat_1','Lng_1','geometry'])
-        res_intersect.rename(columns={'Status_2':'Status',
-                          'UUID_2':'UUID',
-                          'Lat_2':'Lat',
-                          'Lng_2':'Lng'},inplace=True)
+        #res_intersect = overlay(positivePointsBuffered,negativePointsBuffered,how='intersection')
+        #res_intersect=res_intersect.drop(['Status_1','UUID_1','Lat_1','Lng_1','geometry'])
+        res_intersect.rename(columns={'Status':'Status',
+                          'UUID':'UUID',
+                          'Lat':'Lat',
+                          'Lng':'Lng'},inplace=True)
         #res_intersect['UUID_2'],res_intersect['Status_2']
-        res_intersect.drop_duplicates(subset ="UUID",keep = "first", inplace = True)
-        res_intersect['Timeslot']=timestamp_string
+        #res_intersect.drop_duplicates(subset ="UUID",keep = "first", inplace = True)
+        #res_intersect['Timeslot']=timestamp_string
         return res_intersect
 
 
